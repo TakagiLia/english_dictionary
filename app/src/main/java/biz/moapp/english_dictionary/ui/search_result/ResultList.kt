a0dc1cb26a0a8c23fbbdde5c53fd33cf2c89c28a
TakagiRia <r.takagi.test@gmail.com>
2024-09-04 20:06:14 +0900

    Tab要素で表示するResultListコンポーザブルを追加

---
+++ app/src/main/java/biz/moapp/english_dictionary/ui/search_result/ResultList.kt
@@ -0,0 +1,32 @@
+package biz.moapp.english_dictionary.ui.search_result
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ResultList(data: List<String>){
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(4.dp),
+        horizontalAlignment = Alignment.Start
+    ) {
+        items(data) { value ->
+            HorizontalDivider(thickness = 1.dp, color = Color.LightGray)
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "・${value}")
+            Spacer(modifier = Modifier.height(8.dp))
+            HorizontalDivider(thickness = 1.dp, color = Color.LightGray)
+            Spacer(modifier = Modifier.height(1.dp))
+        }
+
+    }
+}
+
