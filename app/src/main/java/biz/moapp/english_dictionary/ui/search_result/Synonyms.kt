f3dfaa8d62a0f67b2b918c7b9e890b078492ba33
TakagiRia <r.takagi.test@gmail.com>
2024-09-04 20:04:28 +0900

    Tab要素Synonymsコンポーザブルの追加

---
+++ app/src/main/java/biz/moapp/english_dictionary/ui/search_result/Synonyms.kt
@@ -0,0 +1,46 @@
+package biz.moapp.english_dictionary.ui.search_result
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import biz.moapp.english_dictionary.DataMock.EnglishSynonym
+
+@Composable
+fun Synonyms(modifier: Modifier = Modifier, data: List<EnglishSynonym>){
+    Column( modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start) {
+        data.forEach { _ ->
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(4.dp),
+                horizontalAlignment = Alignment.Start
+            ) {
+               items(data){
+                   HorizontalDivider(thickness = 1.dp, color = Color.LightGray)
+                   Spacer(modifier = Modifier.height(8.dp))
+                   Row {
+                       Text(text = "英語：${it.eng}, ")
+                       Text(text = "日本語：${it.jp}")
+                   }
+                   Spacer(modifier = Modifier.height(8.dp))
+                   HorizontalDivider(thickness = 1.dp, color = Color.LightGray)
+                   Spacer(modifier = Modifier.height(1.dp))
+               }
+            }
+        }
+    }
+}
