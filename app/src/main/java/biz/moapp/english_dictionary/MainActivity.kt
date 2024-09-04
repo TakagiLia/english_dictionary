cf9a02876bd2ff37e55f9a9e2064a751354712a0
TakagiRia <r.takagi.test@gmail.com>
2024-09-04 15:43:06 +0900

    BaseScreenをSurfaceで囲む

--- app/src/main/java/biz/moapp/english_dictionary/MainActivity.kt
+++ app/src/main/java/biz/moapp/english_dictionary/MainActivity.kt
@@ -6,12 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import biz.moapp.english_dictionary.ui.base.BaseScreen
 import biz.moapp.english_dictionary.ui.theme.English_dictionaryTheme
 import biz.moapp.english_dictionary.ui.top.TopScreenViewModel
@@ -27,7 +24,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             English_dictionaryTheme {
-                BaseScreen(topScreenViewModel)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    BaseScreen(topScreenViewModel)
+                }
             }
         }
     }
