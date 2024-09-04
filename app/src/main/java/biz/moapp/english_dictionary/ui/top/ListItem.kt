27b010b87de2ba09227915e02fb1ebae2af25468
TakagiRia <r.takagi.test@gmail.com>
2024-09-04 15:45:42 +0900

    リスト要素クリックで遷移できるように修正

--- app/src/main/java/biz/moapp/english_dictionary/ui/top/ListItem.kt
+++ app/src/main/java/biz/moapp/english_dictionary/ui/top/ListItem.kt
@@ -18,14 +18,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import biz.moapp.english_dictionary.Language
+import biz.moapp.english_dictionary.navigation.Nav
 
 @Composable
-fun ListItem(data : Language){
+fun ListItem(data : Language, onClick: () -> Unit){
     Row(modifier = Modifier.padding(bottom = 0.5.dp)
         .border(width = 1.dp, color = Color.LightGray, shape = RoundedCornerShape(4.dp))
         .fillMaxWidth()
-        .clickable {},
+        .clickable { onClick() },
     ){
         Spacer(modifier = Modifier.width(8.dp))
         Column {
