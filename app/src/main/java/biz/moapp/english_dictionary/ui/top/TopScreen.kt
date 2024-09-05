f6b8c6a0cc073e5b1779c53e83f6a3fa35de97a4
TakagiRia <r.takagi.test@gmail.com>
2024-09-05 10:06:07 +0900

    検索結果がない場合は「該当する単語がありません」と表示

--- app/src/main/java/biz/moapp/english_dictionary/ui/top/TopScreen.kt
+++ app/src/main/java/biz/moapp/english_dictionary/ui/top/TopScreen.kt
@@ -1,11 +1,13 @@
 package biz.moapp.english_dictionary.ui.top
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
@@ -30,15 +32,24 @@ fun TopScreen(modifier: Modifier = Modifier,
         /**検索バー**/
         SearchBar(modifier, topScreenViewModel)
 
-        /**単語表示**/
-        LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            contentPadding = PaddingValues(horizontal = 8.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            items(filterData.value) { data ->
-                ListItem(data){
-                    navController.navigate("${Nav.SearchResultScreen.name}/${data.englishMean}")
+        if(filterData.value.isEmpty()){
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "該当する単語がありません")
+            }
+        }else {
+            /**単語表示**/
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(horizontal = 8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                items(filterData.value) { data ->
+                    ListItem(data) {
+                        navController.navigate("${Nav.SearchResultScreen.name}/${data.englishMean}")
+                    }
                 }
             }
         }
