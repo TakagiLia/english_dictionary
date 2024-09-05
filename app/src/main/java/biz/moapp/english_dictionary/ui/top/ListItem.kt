fa5207379848cbbce19b16d29e6aee2c7e5fc27b
TakagiRia <r.takagi.test@gmail.com>
2024-09-05 09:16:55 +0900

    不要なインポートとコードの削除

--- app/src/main/java/biz/moapp/english_dictionary/ui/top/ListItem.kt
+++ app/src/main/java/biz/moapp/english_dictionary/ui/top/ListItem.kt
@@ -1,8 +1,6 @@
 package biz.moapp.english_dictionary.ui.top
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,23 +8,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import biz.moapp.english_dictionary.Language
-import biz.moapp.english_dictionary.navigation.Nav
 
 @Composable
 fun ListItem(data : Language, onClick: () -> Unit){
     Row(modifier = Modifier.padding(bottom = 0.5.dp)
-//        .border(width = 1.dp, color = Color.LightGray, shape = RoundedCornerShape(4.dp))
         .fillMaxWidth()
         .clickable { onClick() },
     ){
