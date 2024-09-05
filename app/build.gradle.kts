f28e7f409f7f9886a8604234404da04a8c746a21
TakagiRia <r.takagi.test@gmail.com>
2024-09-05 10:50:37 +0900

    BuildComfigが利用できるよう設定

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     kotlin("kapt")
     alias(libs.plugins.android.application)
@@ -7,6 +9,10 @@ plugins {
 android {
     namespace = "biz.moapp.english_dictionary"
     compileSdk = 34
+
+    buildFeatures {
+        buildConfig = true
+    }
 
     defaultConfig {
         applicationId = "biz.moapp.english_dictionary"
@@ -19,6 +25,10 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
+        val localProperties = Properties()
+        localProperties.load(project.rootProject.file("local.properties").inputStream())
+        buildConfigField("String", "OPENAI_API_KEY", "\"${localProperties.getProperty("OPENAI_API_KEY")}\"")
+        buildConfigField("String", "OPENAI_BASE_URL", "\"${localProperties.getProperty("OPENAI_BASE_URL")}\"")
     }
 
     buildTypes {
