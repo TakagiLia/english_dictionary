3adcb9120d3d58de66bfa1fa6771238b33769709
TakagiRia <r.takagi.test@gmail.com>
2024-09-05 11:06:17 +0900

    不明なインポート先の削除

--- app/src/main/java/biz/moapp/english_dictionary/model/child/ChatChoice.kt
+++ app/src/main/java/biz/moapp/english_dictionary/model/child/ChatChoice.kt
@@ -1,6 +1,5 @@
 package biz.moapp.english_dictionary.model.child
 
-import biz.moapp.transcription_app.model.child.ChatMessage
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
