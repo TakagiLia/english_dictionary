163c10eb274bb024b8e183a6646a61fa037836f2
TakagiRia <r.takagi.test@gmail.com>
2024-09-05 12:04:41 +0900

    hiltライブラリ追加漏れのものを追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     kotlin("kapt")
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.dagger.hilt.android)
 }
 
 android {
