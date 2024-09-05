6c87ff23dff225262267b3745bca1a99eb1e388d
TakagiRia <r.takagi.test@gmail.com>
2024-09-05 09:54:25 +0900

    不要なインポートの削除

--- app/src/main/java/biz/moapp/english_dictionary/ui/search_result/MeanTab.kt
+++ app/src/main/java/biz/moapp/english_dictionary/ui/search_result/MeanTab.kt
@@ -1,6 +1,5 @@
 package biz.moapp.english_dictionary.ui.search_result
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -9,12 +8,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
