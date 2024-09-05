f9bd2dc8604adf77b18869288bf7376d2db6d8f2
TakagiRia <r.takagi.test@gmail.com>
2024-09-05 09:09:45 +0900

    利用してないインんポート削除

--- app/src/main/java/biz/moapp/english_dictionary/ui/top/TopScreen.kt
+++ app/src/main/java/biz/moapp/english_dictionary/ui/top/TopScreen.kt
@@ -3,9 +3,7 @@ package biz.moapp.english_dictionary.ui.top
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
