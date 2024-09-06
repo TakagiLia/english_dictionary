97c6a837d98d5d2f4c3e39a9c0af07677c2f8be0
TakagiRia <r.takagi.test@gmail.com>
2024-09-06 17:38:46 +0900

    APIの通信処理の呼び出しと状態管理の追加

--- app/src/main/java/biz/moapp/english_dictionary/ui/search_result/SearchResultViewModel.kt
+++ app/src/main/java/biz/moapp/english_dictionary/ui/search_result/SearchResultViewModel.kt
@@ -1,8 +1,12 @@
 package biz.moapp.english_dictionary.ui.search_result
 
 import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import biz.moapp.english_dictionary.JsonUtil.convertJsonToWordInfo
 import biz.moapp.english_dictionary.network.RetrofitOpenAiNetwork
 import biz.moapp.english_dictionary.network.model.ChatCompletions
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -24,26 +28,48 @@ class SearchResultViewModel
     private val _jsonData = MutableStateFlow<String>("")
     val jsonData: StateFlow<String> = _jsonData.asStateFlow()
 
+    var resultUiState by mutableStateOf(ResultUiState())
+        private set
+
     fun getEnglishMean(word :String){
         viewModelScope.launch {
+            /**ローディング**/
+            resultUiState = resultUiState.copy(sendResultState = ResultUiState.SendResultState.Loading)
 
+            /**通信処理**/
             runCatching {
                 val result = withContext(ioDispatcher) {
                     retrofitOpenAiNetwork.responseCompletions( retrofitOpenAiNetwork.createJson(word))
                 }
 
-                when (result) {
-                    is ChatCompletions.Response.Success -> {
-                        result.choices.map { value -> value.message?.content?.let{ _jsonData.value = it} }
-                        Log.d("--result response１-","${result.choices.map { it.message?.content }}")
-                    }
+                resultUiState = when (result) {
+                is ChatCompletions.Response.Success -> {
+                    result.choices.map { value -> value.message?.content?.let{ _jsonData.value = it} }
+                    /**受け取ったレスポンスをJsonに整形してWordInfoに変換**/
+                    val wordInfo = convertJsonToWordInfo(result.choices[0].message?.content ?: "")
+                    Log.d("--wordInfo japaneseMeaning","${wordInfo?.japaneseMeaning}")
+                    Log.d("--wordInfo exampleSentences","${wordInfo?.exampleSentences}")
+                    Log.d("--wordInfo synonyms","${wordInfo?.synonyms}")
+                    Log.d("--wordInfo coOccurrences","${wordInfo?.coOccurrences}")
 
-                    is ChatCompletions.Response.Failure -> {
-
-                    }
+                    resultUiState.copy(
+                        result = wordInfo,
+                        sendResultState = ResultUiState.SendResultState.Success(
+                            wordInfo
+                        )
+                    )
                 }
+
+                is ChatCompletions.Response.Failure -> {
+                    resultUiState.copy(
+                        sendResultState = ResultUiState.SendResultState.Error(
+                            result.exception.message ?: "unknown error"
+                        )
+                    )
+                }
+            }
             }.onFailure {  e ->
-                Log.e("--summary Error","Message:${e.message}",e)
+                Log.e("--getEnglishMean Error","Message:${e.message}",e)
             }
         }
     }
