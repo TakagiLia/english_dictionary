17ac0b360f93a7b80e794342ef7a1c33bce6f624
TakagiRia <r.takagi.test@gmail.com>
2024-09-06 17:34:52 +0900

    Jsonに形式を揃えるためのメソッドUtilの追加

---
+++ app/src/main/java/biz/moapp/english_dictionary/JsonUtil.kt
@@ -0,0 +1,39 @@
+package biz.moapp.english_dictionary
+
+import android.util.Log
+import biz.moapp.english_dictionary.data.row.WordInfo
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+
+object JsonUtil {
+    fun convertJsonToWordInfo(rowJson:String) :WordInfo?{
+        try {
+            val json = rowJson
+                .replace("\n", "")
+                .replace("  ", "")
+                .replace("```json", "")
+                .trimIndent()
+
+            Log.d("--convertJsonToWordInfo json", "${json}")
+
+            val moshi = Moshi.Builder()
+                .add(KotlinJsonAdapterFactory())
+                .build()
+
+            val adapter = moshi.adapter(WordInfo::class.java)
+
+            return adapter.fromJson(json)
+        }catch (e:Exception){
+            Log.e("--Error convertJsonToWordInfo","Message: ${e.message}",e)
+        }
+       return null
+    }
+
+    fun convertStringToList(japaneseMeaning :String) :List<String>{
+        return if(japaneseMeaning.contains("; ")){
+            japaneseMeaning.split("; ")
+        }else{
+            listOf(japaneseMeaning)
+        }
+    }
+}
