2e72bf65ad0f09656e1ca55a9e02acdf4d16f802
TakagiRia <r.takagi.test@gmail.com>
2024-09-10 15:13:04 +0900

    MeanTabにSoundアイコンを反映

--- app/src/main/java/biz/moapp/english_dictionary/ui/search_result/parts_compose/tab_content/MeanTab.kt
+++ app/src/main/java/biz/moapp/english_dictionary/ui/search_result/parts_compose/tab_content/MeanTab.kt
@@ -9,23 +9,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import biz.moapp.english_dictionary.R
-import biz.moapp.english_dictionary.ui.search_result.rememberTextToSpeech
+import biz.moapp.english_dictionary.ui.search_result.parts_compose.SoundIcon
 
 @Composable
 fun MeanTab(modifier: Modifier = Modifier, keyWord: String, data: List<String>, textToSpeech: TextToSpeech?,){
-    val tts = rememberTextToSpeech()
     Column( modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.Start){
@@ -35,15 +29,7 @@ fun MeanTab(modifier: Modifier = Modifier, keyWord: String, data: List<String>, 
                 text = keyWord,
                 fontWeight = FontWeight.Bold,
                 fontSize = 40.sp)
-            IconButton(onClick = {
-                tts.value?.speak(keyWord, TextToSpeech.QUEUE_ADD,null, null)
-            },
-                        modifier = Modifier.padding(4.dp),) {
-                Icon(painter = painterResource(R.drawable.volume_up_24px),
-                    contentDescription = "",
-                    tint = MaterialTheme.colorScheme.primary,
-                    )
-            }
+            SoundIcon(keyWord)
         }
         /**意味**/
         Text(modifier = Modifier.padding(4.dp),
