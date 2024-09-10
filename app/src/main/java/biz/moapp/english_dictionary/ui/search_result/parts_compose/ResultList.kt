f2d0a4bc9e4477c5f0a2ccd1503159832519fec1
TakagiRia <r.takagi.test@gmail.com>
2024-09-10 15:09:39 +0900

    新しいSoundアイコンの追加

--- app/src/main/java/biz/moapp/english_dictionary/ui/search_result/parts_compose/ResultList.kt
+++ app/src/main/java/biz/moapp/english_dictionary/ui/search_result/parts_compose/ResultList.kt
@@ -1,26 +1,18 @@
 package biz.moapp.english_dictionary.ui.search_result.parts_compose
 
-import android.speech.tts.TextToSpeech
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import biz.moapp.english_dictionary.R
-import biz.moapp.english_dictionary.ui.search_result.rememberTextToSpeech
 
 @Composable
 fun ResultList(data: List<String>){
-    val tts = rememberTextToSpeech()
 
     LazyColumn(
         modifier = Modifier.fillMaxSize(),
@@ -30,16 +22,8 @@ fun ResultList(data: List<String>){
         items(data) { value ->
             Spacer(modifier = Modifier.height(8.dp))
             Row {
-                Text(text = "・${value}")
-                IconButton(onClick = {
-                    tts.value?.speak(value, TextToSpeech.QUEUE_ADD,null, null)
-                },
-                    modifier = Modifier.padding(4.dp),) {
-                    Icon(painter = painterResource(R.drawable.volume_up_24px),
-                        contentDescription = "",
-                        tint = MaterialTheme.colorScheme.primary,
-                    )
-                }
+                SoundIcon(value)
+                Text(text = value)
             }
             Spacer(modifier = Modifier.height(8.dp))
             HorizontalDivider(thickness = 1.dp, color = Color.LightGray)
