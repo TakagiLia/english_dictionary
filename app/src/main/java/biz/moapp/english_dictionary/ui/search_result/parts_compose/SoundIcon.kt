4d49d348d3e5162cbc46723f2dc6e8bb57515d8d
TakagiRia <r.takagi.test@gmail.com>
2024-09-10 15:46:34 +0900

    サウンドアイコンのリップル削除

--- app/src/main/java/biz/moapp/english_dictionary/ui/search_result/parts_compose/SoundIcon.kt
+++ app/src/main/java/biz/moapp/english_dictionary/ui/search_result/parts_compose/SoundIcon.kt
@@ -1,24 +1,24 @@
 package biz.moapp.english_dictionary.ui.search_result.parts_compose
 
 import android.speech.tts.TextToSpeech
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import biz.moapp.english_dictionary.R
 import biz.moapp.english_dictionary.ui.search_result.rememberTextToSpeech
 
 @Composable
 fun SoundIcon(word :String){
     val tts = rememberTextToSpeech()
-    IconButton(onClick = {
-        tts.value?.speak(word, TextToSpeech.QUEUE_ADD,null, null)
-    },
-        modifier = Modifier.padding(4.dp),) {
+    Row(modifier = Modifier.clickable(onClick = { tts.value?.speak(word, TextToSpeech.QUEUE_ADD,null, null) },
+        indication = null, //indication = nullでリップル削除
+        interactionSource = remember { MutableInteractionSource() },)){
         Icon(painter = painterResource(R.drawable.volume_up_24px),
             contentDescription = "",
             tint = MaterialTheme.colorScheme.primary,
