caceb3a2f50a9f6fc42b933e37feee8ddcbab4f9
TakagiRia <r.takagi.test@gmail.com>
2024-09-10 14:51:49 +0900

    rememberTextToSpeech追加

--- app/src/main/java/biz/moapp/english_dictionary/ui/search_result/parts_compose/ResultList.kt
+++ app/src/main/java/biz/moapp/english_dictionary/ui/search_result/parts_compose/ResultList.kt
@@ -1,18 +1,32 @@
 package biz.moapp.english_dictionary.ui.search_result.parts_compose
 
+import android.speech.tts.TextToSpeech
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import biz.moapp.english_dictionary.R
+import biz.moapp.english_dictionary.ui.search_result.rememberTextToSpeech
+import java.util.Locale
 
 @Composable
 fun ResultList(data: List<String>){
+    val tts = rememberTextToSpeech()
+
     LazyColumn(
         modifier = Modifier.fillMaxSize(),
         contentPadding = PaddingValues(4.dp),
@@ -20,7 +34,18 @@ fun ResultList(data: List<String>){
     ) {
         items(data) { value ->
             Spacer(modifier = Modifier.height(8.dp))
-            Text(text = "・${value}")
+            Row {
+                Text(text = "・${value}")
+                IconButton(onClick = {
+                    tts.value?.speak(value, TextToSpeech.QUEUE_ADD,null, null)
+                },
+                    modifier = Modifier.padding(4.dp),) {
+                    Icon(painter = painterResource(R.drawable.volume_up_24px),
+                        contentDescription = "",
+                        tint = MaterialTheme.colorScheme.primary,
+                    )
+                }
+            }
             Spacer(modifier = Modifier.height(8.dp))
             HorizontalDivider(thickness = 1.dp, color = Color.LightGray)
             Spacer(modifier = Modifier.height(1.dp))
