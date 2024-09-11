a80256305780ced1b52138db585b8112ebbf08e1
TakagiRia <r.takagi.test@gmail.com>
2024-09-11 14:56:47 +0900

    一度APIに投げたものは検索履歴持っておく

--- app/src/main/java/biz/moapp/english_dictionary/ui/search_result/SearchResultViewModel.kt
+++ app/src/main/java/biz/moapp/english_dictionary/ui/search_result/SearchResultViewModel.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import biz.moapp.english_dictionary.data.json_row.WordInfo
 import biz.moapp.english_dictionary.utill.JsonUtil.convertJsonToWordInfo
 import biz.moapp.english_dictionary.network.RetrofitOpenAiNetwork
 import biz.moapp.english_dictionary.network.model.ChatCompletions
@@ -28,6 +29,9 @@ class SearchResultViewModel
     private val _jsonData = MutableStateFlow<String>("")
     val jsonData: StateFlow<String> = _jsonData.asStateFlow()
 
+    private val _searchHistory = MutableStateFlow<Map<String, WordInfo>>(emptyMap())
+    val searchHistory: StateFlow<Map<String, WordInfo>> = _searchHistory.asStateFlow()
+
     var resultUiState by mutableStateOf(ResultUiState())
         private set
 
@@ -36,40 +40,66 @@ class SearchResultViewModel
             /**ローディング**/
             resultUiState = resultUiState.copy(sendResultState = ResultUiState.SendResultState.Loading)
 
-            /**通信処理**/
-            runCatching {
-                val result = withContext(ioDispatcher) {
-                    retrofitOpenAiNetwork.responseCompletions( retrofitOpenAiNetwork.createJson(word))
-                }
+                /**過去に検索したものか**/
+                val searchFlag = _searchHistory.value.containsKey(word)
 
-                resultUiState = when (result) {
-                is ChatCompletions.Response.Success -> {
-                    result.choices.map { value -> value.message?.content?.let{ _jsonData.value = it} }
-                    /**受け取ったレスポンスをJsonに整形してWordInfoに変換**/
-                    val wordInfo = convertJsonToWordInfo(result.choices[0].message?.content ?: "")
-                    Log.d("--wordInfo japaneseMeaning","${wordInfo?.japaneseMeaning}")
-                    Log.d("--wordInfo exampleSentences","${wordInfo?.exampleSentences}")
-                    Log.d("--wordInfo synonyms","${wordInfo?.synonyms}")
-                    Log.d("--wordInfo coOccurrences","${wordInfo?.coOccurrences}")
-
-                    resultUiState.copy(
-                        result = wordInfo,
+                if (searchFlag) {
+                    /**履歴から意味を取得**/
+                    resultUiState = resultUiState.copy(
+                        result = _searchHistory.value[word],
                         sendResultState = ResultUiState.SendResultState.Success(
-                            wordInfo
+                            _searchHistory.value[word]
                         )
                     )
-                }
+                } else {
+                    /**検索の通信処理**/
+                    runCatching {
+                        val result = withContext(ioDispatcher) {
+                            retrofitOpenAiNetwork.responseCompletions(
+                                retrofitOpenAiNetwork.createJson(
+                                    word
+                                )
+                            )
+                        }
+                        /**処理の結果によってUI反映状態を変更**/
+                        resultUiState = when (result) {
+                            is ChatCompletions.Response.Success -> {
+                                result.choices.map { value ->
+                                    value.message?.content?.let {
+                                        _jsonData.value = it
+                                    }
+                                }
+                                /**受け取ったレスポンスをJsonに整形してWordInfoに変換**/
+                                val wordInfo =
+                                    convertJsonToWordInfo(result.choices[0].message?.content ?: "")
+                                /**一度検索したものは履歴として残す**/
+                                wordInfo?.let {
+                                    _searchHistory.value = mutableMapOf(word to it)
+                                }
+                                Log.d("--wordInfo japaneseMeaning", "${wordInfo?.japaneseMeaning}")
+                                Log.d("--wordInfo exampleSentences", "${wordInfo?.exampleSentences}")
+                                Log.d("--wordInfo synonyms", "${wordInfo?.synonyms}")
+                                Log.d("--wordInfo coOccurrences", "${wordInfo?.coOccurrences}")
 
-                is ChatCompletions.Response.Failure -> {
-                    resultUiState.copy(
-                        sendResultState = ResultUiState.SendResultState.Error(
-                            result.exception.message ?: "unknown error"
-                        )
-                    )
-                }
-            }
-            }.onFailure {  e ->
-                Log.e("--getEnglishMean Error","Message:${e.message}",e)
+                                resultUiState.copy(
+                                    result = wordInfo,
+                                    sendResultState = ResultUiState.SendResultState.Success(
+                                        wordInfo
+                                    )
+                                )
+                            }
+
+                            is ChatCompletions.Response.Failure -> {
+                                resultUiState.copy(
+                                    sendResultState = ResultUiState.SendResultState.Error(
+                                        result.exception.message ?: "unknown error"
+                                    )
+                                )
+                            }
+                        }
+                    }.onFailure { e ->
+                        Log.e("--getEnglishMean Error", "Message:${e.message}", e)
+                    }
             }
         }
     }
