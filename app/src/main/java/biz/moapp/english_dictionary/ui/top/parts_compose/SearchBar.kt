b824847a080cb02d3478af07a6d9e427e2ecd3ae
TakagiRia <r.takagi.test@gmail.com>
2024-09-12 14:49:01 +0900

    検索入力欄にクリアボタンの実装

--- app/src/main/java/biz/moapp/english_dictionary/ui/top/parts_compose/SearchBar.kt
+++ app/src/main/java/biz/moapp/english_dictionary/ui/top/parts_compose/SearchBar.kt
@@ -1,20 +1,18 @@
 package biz.moapp.english_dictionary.ui.top.parts_compose
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.horizontalScroll
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Cancel
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,14 +22,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import biz.moapp.english_dictionary.R
 import biz.moapp.english_dictionary.ui.top.TopScreenViewModel
 
@@ -42,7 +36,9 @@ fun SearchBar(modifier: Modifier = Modifier,
 
     var text by remember { mutableStateOf("") }
 
-    Row(modifier = Modifier.padding(horizontal = 8.dp),
+    Row(modifier = Modifier
+        .padding(horizontal = 8.dp)
+        .fillMaxSize(),
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.Top){
         OutlinedTextField(
@@ -62,6 +58,15 @@ fun SearchBar(modifier: Modifier = Modifier,
                     Text(stringResource(R.string.search_bar_placeholders),)
                 }
             },
+            trailingIcon = {
+                if(text.isNotEmpty()){
+                    Icon(imageVector = Icons.Filled.Cancel,
+                        modifier = Modifier.clickable(onClick = {  },
+                            indication = null, //indication = nullでリップル削除
+                            interactionSource = remember { MutableInteractionSource() },),
+                        contentDescription = "")
+                }
+            }
         )
     }
 
