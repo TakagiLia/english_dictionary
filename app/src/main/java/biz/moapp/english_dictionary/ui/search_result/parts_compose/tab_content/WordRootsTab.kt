a1169c87b3d78c6f16f05acb8c65300f37ad5502
TakagiRia <r.takagi.test@gmail.com>
2024-09-13 15:57:30 +0900

    WordRootsTabに取得した語源の内容をUIに反映

--- app/src/main/java/biz/moapp/english_dictionary/ui/search_result/parts_compose/tab_content/WordRootsTab.kt
+++ app/src/main/java/biz/moapp/english_dictionary/ui/search_result/parts_compose/tab_content/WordRootsTab.kt
@@ -3,16 +3,18 @@ package biz.moapp.english_dictionary.ui.search_result.parts_compose.tab_content
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun WordRootsTab(){
+fun WordRootsTab(text:String){
     Column( modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.Start) {
-        Text("語源")
+        Text(text = text, modifier = Modifier.padding(top = 16.dp))
     }
 }
