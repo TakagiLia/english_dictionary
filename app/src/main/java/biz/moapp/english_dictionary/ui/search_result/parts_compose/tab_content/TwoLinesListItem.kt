ff604556db5dc238e3f3ad50588118e5492fbd6d
TakagiRia <r.takagi.test@gmail.com>
2024-09-13 17:02:58 +0900

    二行のリスト要素のコンポーザブルの追加

---
+++ app/src/main/java/biz/moapp/english_dictionary/ui/search_result/parts_compose/tab_content/TwoLinesListItem.kt
@@ -0,0 +1,32 @@
+package biz.moapp.english_dictionary.ui.search_result.parts_compose.tab_content
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import biz.moapp.english_dictionary.R
+import biz.moapp.english_dictionary.ui.search_result.parts_compose.SoundIcon
+
+@Composable
+fun TwoLinesListItem(word: String, japaneseMeaning: String ){
+    Spacer(modifier = Modifier.height(8.dp))
+    Row {
+        SoundIcon(word = word)
+        Column(modifier = Modifier.padding(top = 2.dp)) {
+            Text(text = stringResource(R.string.synonyms_eng_label) + word)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = stringResource(R.string.synonyms_jp_label) + japaneseMeaning)
+        }
+    }
+    Spacer(modifier = Modifier.height(8.dp))
+    HorizontalDivider(thickness = 1.dp, color = Color.LightGray)
+    Spacer(modifier = Modifier.height(1.dp))
+}
