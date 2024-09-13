4765bfde1c5a1e2cc2d41a8ada4a8d6bc90f4e30
TakagiRia <r.takagi.test@gmail.com>
2024-09-13 15:56:38 +0900

    AntonymsTabに取得した反対語をUIに反映

--- app/src/main/java/biz/moapp/english_dictionary/ui/search_result/parts_compose/tab_content/AntonymsTab.kt
+++ app/src/main/java/biz/moapp/english_dictionary/ui/search_result/parts_compose/tab_content/AntonymsTab.kt
@@ -2,17 +2,52 @@ package biz.moapp.english_dictionary.ui.search_result.parts_compose.tab_content
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import biz.moapp.english_dictionary.R
+import biz.moapp.english_dictionary.data.json_row.Antonyms
+import biz.moapp.english_dictionary.ui.search_result.parts_compose.SoundIcon
 
 @Composable
-fun AntonymsTab(){
+fun AntonymsTab(modifier: Modifier = Modifier, synonyms: List<Antonyms>){
     Column( modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.Start) {
-        Text("反対後")
+        synonyms.forEach { _ ->
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(4.dp),
+                horizontalAlignment = Alignment.Start
+            ) {
+                items(synonyms){ synonym ->
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Row {
+                        SoundIcon(word = synonym.word)
+                        Column(modifier = Modifier.padding(top = 2.dp)) {
+                            Text(text = stringResource(R.string.synonyms_eng_label) + synonym.word)
+                            Spacer(modifier = Modifier.height(4.dp))
+                            Text(text = stringResource(R.string.synonyms_jp_label) + synonym.japaneseMeaning)
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(8.dp))
+                    HorizontalDivider(thickness = 1.dp, color = Color.LightGray)
+                    Spacer(modifier = Modifier.height(1.dp))
+                }
+            }
+        }
     }
 }
