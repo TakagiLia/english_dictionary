87c0369b3522be4c2ff4f577bd6aef99fb4fca3f
TakagiRia <r.takagi.test@gmail.com>
2024-09-20 16:20:13 +0900

    文字を押下すると英語が読み上げられるように修正

--- app/src/main/java/biz/moapp/english_dictionary/ui/search_result/parts_compose/SoundIcon.kt
+++ app/src/main/java/biz/moapp/english_dictionary/ui/search_result/parts_compose/SoundIcon.kt
@@ -1,14 +1,10 @@
 package biz.moapp.english_dictionary.ui.search_result.parts_compose
 
 import android.speech.tts.TextToSpeech
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -16,16 +12,9 @@ import biz.moapp.english_dictionary.R
 
 @Composable
 fun SoundIcon(word :String, modifier: Modifier = Modifier, tts: TextToSpeech?){
-    val onClick = remember(tts, word) {
-        { tts?.speak(word, TextToSpeech.QUEUE_ADD, null, null) }
-    }
-    Row(modifier = modifier.clickable(onClick = { onClick() },
-        indication = null, //indication = nullでリップル削除
-        interactionSource = remember { MutableInteractionSource() },)){
         Icon(painter = painterResource(R.drawable.volume_up_24px),
             contentDescription = "",
             tint = MaterialTheme.colorScheme.primary,
             modifier = Modifier.size(32.dp)
         )
-    }
 }
--- app/src/main/java/biz/moapp/english_dictionary/ui/search_result/parts_compose/tab_content/MeanTab.kt
+++ app/src/main/java/biz/moapp/english_dictionary/ui/search_result/parts_compose/tab_content/MeanTab.kt
@@ -1,6 +1,8 @@
 package biz.moapp.english_dictionary.ui.search_result.parts_compose.tab_content
 
 import android.speech.tts.TextToSpeech
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,6 +11,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -18,8 +21,14 @@ import biz.moapp.english_dictionary.ui.search_result.parts_compose.SoundIcon
 
 @Composable
 fun MeanTab(modifier: Modifier = Modifier, keyWord: String, data: List<String>, tts: TextToSpeech?,){
+    val onClick = remember(tts, keyWord) {
+        { tts?.speak(keyWord, TextToSpeech.QUEUE_ADD, null, null) }
+    }
+
     /**英単語**/
-    Row {
+    Row(modifier = Modifier.clickable(onClick = { onClick() },
+        indication = null,
+        interactionSource = remember { MutableInteractionSource() },)) {
         Text(modifier = Modifier.padding(4.dp),
             text = keyWord,
             fontWeight = FontWeight.Bold,
