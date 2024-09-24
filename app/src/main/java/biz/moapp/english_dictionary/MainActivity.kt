8cfc26bbb022bdf05cc4ec01b3c764fad2385429
TakagiRia <r.takagi.test@gmail.com>
2024-09-24 13:23:02 +0900

    Activityで広告初期化

--- app/src/main/java/biz/moapp/english_dictionary/MainActivity.kt
+++ app/src/main/java/biz/moapp/english_dictionary/MainActivity.kt
@@ -1,6 +1,7 @@
 package biz.moapp.english_dictionary
 
 import android.os.Bundle
+import android.util.DisplayMetrics
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -9,16 +10,30 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import biz.moapp.english_dictionary.ui.base.BaseScreen
 import biz.moapp.english_dictionary.ui.search_result.SearchResultViewModel
 import biz.moapp.english_dictionary.ui.theme.English_dictionaryTheme
 import biz.moapp.english_dictionary.ui.top.TopScreenViewModel
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        /**AdMob初期化**/
+        MobileAds.initialize(this)
+
+        /**AdMob　バナー広告**/
+        val banner = AdView(this)
+        banner.setAdSize(AdSize.BANNER)
+        banner.adUnitId = getString(R.string.banner_ad_id_test)
+        banner.loadAd(AdRequest.Builder().build())
 
         /**AssetからCSV読み込み**/
         val topScreenViewModel: TopScreenViewModel by viewModels()
@@ -33,7 +48,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    BaseScreen(topScreenViewModel, searchResultViewModel,)
+                    BaseScreen(topScreenViewModel, searchResultViewModel, banner)
                 }
             }
         }
