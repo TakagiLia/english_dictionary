c8a3a3ba772f62588db025cb6091699d339f579a
TakagiRia <r.takagi.test@gmail.com>
2024-09-24 16:49:57 +0900

    TopScreenの不要ライブラリ削除

--- app/src/main/java/biz/moapp/english_dictionary/ui/top/TopScreen.kt
+++ app/src/main/java/biz/moapp/english_dictionary/ui/top/TopScreen.kt
@@ -1,13 +1,11 @@
 package biz.moapp.english_dictionary.ui.top
 
-import android.widget.FrameLayout
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,13 +14,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavController
 import biz.moapp.english_dictionary.R
 import biz.moapp.english_dictionary.ui.top.parts_compose.BannerAds
 import biz.moapp.english_dictionary.ui.top.parts_compose.ScrollBarList
 import biz.moapp.english_dictionary.ui.top.parts_compose.SearchBar
-import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
 
 @Composable
