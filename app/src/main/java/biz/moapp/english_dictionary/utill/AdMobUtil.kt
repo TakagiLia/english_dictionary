b17de305b132d62051217f243a81cd9cc5b986b2
TakagiRia <r.takagi.test@gmail.com>
2024-09-24 16:48:09 +0900

    広告IDを本番かデバッグか分ける

---
+++ app/src/main/java/biz/moapp/english_dictionary/utill/AdMobUtil.kt
@@ -0,0 +1,25 @@
+package biz.moapp.english_dictionary.utill
+
+import biz.moapp.english_dictionary.BuildConfig
+
+/**デバッグの場合はテスト用、本番では本番用IDを利用する様にする**/
+object AdMobUtil {
+
+    /**バナー広告IDの取得**/
+    fun getBannerAdUnitId(): String {
+        return if (BuildConfig.DEBUG) {
+            "ca-app-pub-3940256099942544/6300978111"
+        } else {
+            "ca-app-pub-5071120464333172/7299465232"
+        }
+    }
+
+    /**ネイティブ広告IDの取得**/
+    fun getNativeAdUnitId(): String {
+        return if (BuildConfig.DEBUG) {
+            "ca-app-pub-3940256099942544/2247696110"
+        } else {
+            "ca-app-pub-5071120464333172/6206709563"
+        }
+    }
+}
