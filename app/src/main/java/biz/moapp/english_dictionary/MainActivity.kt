52c6389a9b5351b3543a2482d8785655bf8b3e37
TakagiRia <r.takagi.test@gmail.com>
2024-09-24 16:17:11 +0900

    ネイティブ広告の初期化を追加

--- app/src/main/java/biz/moapp/english_dictionary/MainActivity.kt
+++ app/src/main/java/biz/moapp/english_dictionary/MainActivity.kt
@@ -2,6 +2,8 @@ package biz.moapp.english_dictionary
 
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.util.Log
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,18 +13,25 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.databinding.DataBindingUtil
+import biz.moapp.english_dictionary.databinding.NativeAdLayoutBinding
 import biz.moapp.english_dictionary.ui.base.BaseScreen
 import biz.moapp.english_dictionary.ui.search_result.SearchResultViewModel
 import biz.moapp.english_dictionary.ui.theme.English_dictionaryTheme
 import biz.moapp.english_dictionary.ui.top.TopScreenViewModel
+import com.google.android.gms.ads.AdLoader
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.nativead.NativeAdView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private var nativeAdLayoutBinding: NativeAdLayoutBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -34,6 +43,9 @@ class MainActivity : ComponentActivity() {
         banner.setAdSize(AdSize.BANNER)
         banner.adUnitId = getString(R.string.banner_ad_id_test)
         banner.loadAd(AdRequest.Builder().build())
+
+        /**ネイティブ広告の初期化**/
+        initNativeAd()
 
         /**AssetからCSV読み込み**/
         val topScreenViewModel: TopScreenViewModel by viewModels()
@@ -48,9 +60,40 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    BaseScreen(topScreenViewModel, searchResultViewModel, banner)
+                    BaseScreen(topScreenViewModel, searchResultViewModel, banner,
+                        nativeAdView = nativeAdLayoutBinding!!.root as NativeAdView)
                 }
             }
         }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        /**広告の解放**/
+        (nativeAdLayoutBinding?.root as? NativeAdView)?.destroy()
+    }
+
+    /**ネイティブ広告の初期化**/
+    private fun initNativeAd() {
+        nativeAdLayoutBinding = DataBindingUtil.inflate(
+            layoutInflater,
+            R.layout.native_ad_layout,
+            null,
+            false
+        )
+        AdLoader.Builder(this, getString(R.string.admob_native_unit_id_test))
+            .forNativeAd { ad ->
+                val nativeAdView = nativeAdLayoutBinding!!.root as NativeAdView
+
+                nativeAdView.mediaView = nativeAdLayoutBinding!!.adMedia
+                nativeAdLayoutBinding!!.adMedia.setImageScaleType(ImageView.ScaleType.FIT_CENTER)
+                nativeAdView.headlineView = nativeAdLayoutBinding!!.adHeadline
+                nativeAdLayoutBinding!!.adHeadline.text = ad.headline
+                nativeAdView.bodyView = nativeAdLayoutBinding!!.adBody
+                nativeAdLayoutBinding!!.adBody.text = ad.body
+                nativeAdView.adChoicesView = nativeAdLayoutBinding!!.adChoices
+                nativeAdView.advertiserView = nativeAdLayoutBinding!!.adAdvertiser
+                nativeAdLayoutBinding!!.adAdvertiser.text = ad.advertiser
+                nativeAdView.setNativeAd(ad)
+            }.build().loadAd(AdRequest.Builder().build())
     }
 }
